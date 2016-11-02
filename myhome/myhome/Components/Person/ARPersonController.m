71d63203f90fd0d159103290db0fe7c554e2d9c9
fudondong <fudongdong@guazi.com>
2016-11-02 11:00:37 +0800

    添加工程

---
+++ myhome/myhome/Components/Person/ARPersonController.m
@@ -0,0 +1,37 @@
+//
+//  ARPersonController.m
+//  myhome
+//
+//  Created by fudon on 2016/11/1.
+//  Copyright © 2016年 fuhope. All rights reserved.
+//
+
+#import "ARPersonController.h"
+
+@interface ARPersonController ()
+
+@end
+
+@implementation ARPersonController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.title = NSStringFromClass([self class]);
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
