0b13434447f1dce9ebec9980e0ed26c56375acf8
fudongdong <fudongdong@guazi.com>
2017-07-12 11:50:59 +0800

    7.12 11:50

---
+++ myhome/myhome/FSShopClassView.h
@@ -0,0 +1,13 @@
+//
+//  FSShopClassView.h
+//  myhome
+//
+//  Created by Guazi on 2017/7/12.
+//  Copyright © 2017年 fuhope. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FSShopClassView : UIView
+
+@end
---
+++ myhome/myhome/FSShopClassView.m
@@ -0,0 +1,21 @@
+//
+//  FSShopClassView.m
+//  myhome
+//
+//  Created by Guazi on 2017/7/12.
+//  Copyright © 2017年 fuhope. All rights reserved.
+//
+
+#import "FSShopClassView.h"
+
+@implementation FSShopClassView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
